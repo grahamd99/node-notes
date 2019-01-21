bbb2dc9fe18e0df15ac267317534d2048876c32a
Graham Dodsworth <31283488+grahamd99@users.noreply.github.com>
2019-01-21 21:17:01 +0000

    Create note.js

---
+++ note.js
@@ -0,0 +1,18 @@
+console.log('Starting notes.js');
+
+module.exports.age = 25;
+
+module.exports.add = (a,b) => {
+  console.log('add');
+  return a+b;
+};
+
+
+/*
+module.exports.addNote = () => {
+  console.log('addNote');
+  return 'New note';
+};
+*/
+
+//console.log(module);
