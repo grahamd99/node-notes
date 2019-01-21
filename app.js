7eefb012d1f5cf2e17b26daee6807cc445408140
grahamd99 <graham.dodsworth@nhs.net>
2019-01-21 22:23:29 +0000

    lodash added, with example, plus gitignore

--- app.js
+++ app.js
@@ -2,7 +2,11 @@ console.log('Starting app.js');
 
 const fs = require('fs');
 const os = require('os');
+const _ = require('lodash');
 const notes = require('./notes.js');
+
+console.log(_.isString(true));
+console.log(_.isString('Gary'));
 
 var addup = notes.add(8,-3);
 console.log(addup);
